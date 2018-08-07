8e6b573be89a40f143a4cc13e6ac6ec07f350105
Erick Merchant <erickmerchant@gmail.com>
2018-08-07 07:55:05 -0700

    Use a for loop

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ module.exports = function classlist (prefix = '') {
           results.push(classlist(prefix)(v))
         }
       } else {
-        for (const k of Object.keys(arg)) {
+        const keys = Object.keys(arg)
+        for (let i = 0; i < keys.length; i++) {
+          const k = keys[i]
           const r = classlist(prefix + k)(arg[k])
 
           if (r) {
